19a1d9345755870c6249219ca6554f6ed5c8a082
JessAthey <jelaineya@gmail.com>
2022-09-12 09:26:40 -0400

    changes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ResultsObj from "./components/Results";
+import ButtonDispay from "./components/ButtonDisplay";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+    if(button === "="){
+      this.calculate()
+    } else if(button === "C"){
+      this.delete()
+    } else if(button === "CE"){
+      this.clear()
+    }else{
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+  calculate = () => {
+    try{
+      this.setState({
+        result: (
+          eval(this.state.result) || "" ) + ""
+      })
+    } catch(e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  clear = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  delete = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
+  render(){
+    return(
+      <div>
+        <div className="calcBody">
+          <h1>React Calculator</h1>
+          <ResultsObj result={this.state.result}/>
+          <ButtonDispay onClick={this.onClick}/>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
